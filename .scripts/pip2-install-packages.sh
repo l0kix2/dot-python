09ae2c0081991b917355e18ef431bfde9f834e3b
Kirill SIbirev <l0kix2@gmail.com>
2019-09-24 15:48:11 +0300

    Update pip2-install-packages.sh

--- .scripts/pip2-install-packages.sh
+++ .scripts/pip2-install-packages.sh
@@ -1,3 +1,3 @@
 pip2 install --upgrade pip
-pip2 list --format=freeze 2> /dev/null | diff $HOME/.python-packages/py2-packages.txt - | grep '>' | grep -v pip | grep -v setuptools | awk '{print $2}' | awk -F '==' '{print $1}' | xargs -I{} pip2 uninstall {}
-pip2 list --format=freeze 2> /dev/null | diff $HOME/.python-packages/py2-packages.txt - | grep '<' | grep -v '#' | awk '{print $2}' | xargs -I{} pip2 install {}
+pip2 list --format=freeze 2> /dev/null | diff $HOME/.conf/py2-packages.txt - | grep '>' | grep -v pip | grep -v setuptools | awk '{print $2}' | awk -F '==' '{print $1}' | xargs -I{} pip2 uninstall {}
+pip2 list --format=freeze 2> /dev/null | diff $HOME/.conf/py2-packages.txt - | grep '<' | grep -v '#' | awk '{print $2}' | xargs -I{} pip2 install {}
